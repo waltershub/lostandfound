c04a5c34d129d7b5a80cac938ede011da921bf1d
YuChao Dong <yuchaodong@gmail.com>
2017-08-01 17:01:21 -0400

    added lostItems skeleton

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import MenuBar from './MenuBar.jsx';
+import LostItems from './LostItemsList.jsx';
 
 class App extends React.Component {
   render() {
     return (
-      <MenuBar />
+      <div>
+        <MenuBar />
+        <LostItems />
+      </div>
     );
   }
 }
