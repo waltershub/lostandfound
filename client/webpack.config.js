c75f9a8818f572a7b999a4fc2ffcca386032b1f0
YuChao Dong <yuchaodong@gmail.com>
2017-08-01 12:36:47 -0400

    complete frontend setup

---
+++ client/webpack.config.js
@@ -0,0 +1,29 @@
+const config = {
+  entry: './main.jsx',
+
+  output: {
+    path: './',
+    filename: 'index.js'
+  },
+
+  devServer: {
+    inline: true,
+    port: 3000
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
