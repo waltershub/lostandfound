5733f57c28b57eba882d7112ba602864392950ed
Alec Draymore <draymore26@gmail.com>
2017-08-02 11:26:19 -0400

    login v2

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,25 +1,63 @@
 import React from 'react';
-
-var input = "blank";
+import axios from 'axios';
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      username: '',
+      password: '',
+    };
+
     this.submitLogin = this.submitLogin.bind(this);
+    this.setUsername = this.setUsername.bind(this);
+    this.setPassword = this.setPassword.bind(this);
   }
-  submitLogin(input) {
-    window.alert('input', input);
+
+  setUsername(e) {
+    this.setState({ username: e.target.value });
   }
+
+  setPassword(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  submitLogin() {
+    axios.get('/user', {
+      params: {
+        username: this.state.username,
+        password: this.state.password,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
   render() {
     return (
       <div>
         <h2>Login</h2>
         <form onSubmit={this.submitLogin}>
           <p>username</p>
-          <input type="text" />
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.setUsername}
+          />
 
           <p>password</p>
-          <input type="password" />
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.setPassword}
+          />
 
           <button type="submit">Submit</button>
         </form>
