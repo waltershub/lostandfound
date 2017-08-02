10f8ea1b806b4302cbac6f0f072d32cb5d3461af
Alec Draymore <draymore26@gmail.com>
2017-08-02 16:29:17 -0400

    route refactor working version

--- server/lib/routes.js
+++ server/lib/routes.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const signupHandler = require('../lib/controllers/signupHandler');
+//const loginHandler = require('../lib/controllers/loginHandler');
 const requestHandlers = require('./requestHandlers');
-const { auth } = require('./controllers/auth');
+//const { auth } = require('./controllers/auth');
 
 router.post('/signup', signupHandler);
 
-router.post('/login', auth, (req, res) => {
-
-});
+//router.get('/login', loginHandler);
 
 router.post('/lostitem', requestHandlers.postLostItem);
-router.post('/founditem', requestHandlers.postFoundItem);
+// router.post('/founditem', requestHandlers.postFoundItem);
 
-router.post('/messages', requestHandlers.postMessages);
-router.get('/messages', requestHandlers.getMessages);
+// router.post('/messages', requestHandlers.postMessages);
+// router.get('/messages', requestHandlers.getMessages);
 
-router.get('/matches', requestHandlers.getMatches);
+// router.get('/matches', requestHandlers.getMatches);
 
-router.get('/dashdata', requestHandlers.getDashData);
+// router.get('/dashdata', requestHandlers.getDashData);
 
 module.exports.router = router;
