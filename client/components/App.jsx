890f105f8a0bc9bc38d9abdf12eb991f0ae13ca7
Alec Draymore <draymore26@gmail.com>
2017-08-02 10:34:24 -0400

    set up front-end routing with auth skeleton

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import DashBoard from './DashBoard.jsx';
+import TestComp from './TestComp.jsx';
+
+const fakeAuth = {
+  loggedIn: true,
+  login() {
+    this.loggedIn = true;
+  },
+  logout() {
+    this.loggedIn = true;
+  },
+};
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <Route path="/" />
-        <Route path="/dash" component={DashBoard} />
-        <Route path="/itemform">
-          <Route path="/lost" />
-          <Route path="/found" />
-        </Route>
-        <Route path="/login" />
-        <Route path="/signup" />
+        <div>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              fakeAuth.loggedIn ? (
+                <Redirect to="/dash" />
+              ) : (
+                //add login comp
+              )
+            )}
+          />
+          <Route path="/dash" component={DashBoard} />
+        </div>
       </Router>
     );
   }
