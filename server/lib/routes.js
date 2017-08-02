238ae3b6f8d90c9af4e3bba77c9511c9a8a5a355
Alec Draymore <draymore26@gmail.com>
2017-08-02 16:16:37 -0400

    refactored

--- server/lib/routes.js
+++ server/lib/routes.js
@@ -1,29 +1,16 @@
 const express = require('express');
-const db = require('../../db/config');
-const controller = require('../lib/controllers/controllers');
 const router = express.Router();
-const {auth} = require('../lib/controller/auth')
+const signupHandler = require('../lib/controllers/signupHandler');
+const requestHandlers = require('./requestHandlers');
+const { auth } = require('./controllers/auth');
 
-router.post('/', (req, res) => {
-  console.log('success on index');
-  res.sendFile('../../client/index.html');
-});
+router.post('/signup', signupHandler);
 
-
-router.post('/signup', controller.signup);
-
-router.post('/login', auth, (null, user) =>{
+router.post('/login', auth, (err, user) =>{
   res.send(null , user)
 });
 
-router.post('/lostitem', (req, res) => {
-  db.lost.create(req.body, (err) => {
-    if (err) throw err;
-
-    // must still write here check data base as a call back
-  });
-  res.send('success on post lost item');
-});
+router.post('/lostitem', requestHandlers.postLostItem);
 
 
 module.exports.router = router;
