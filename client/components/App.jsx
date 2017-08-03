a400a760d5e063b393863f04b312160912fd1c94
Chetanpreet Malhi <chemalhi@gmail.com>
2017-08-03 10:40:30 -0400

    Messages are now showing up on the screen, working on post and get request

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import axios from 'axios';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import DashBoard from './DashBoard.jsx';
 import Login from './Login.jsx';
 import ItemForm from './ItemForm.jsx';
 import Master from './Master.jsx';
+import Messenger from './Messenger.jsx';
+
 
 const fakeAuth = {
   loggedIn: false,
@@ -17,29 +18,24 @@ const fakeAuth = {
 };
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state ={
-      isLoggedIn: false,
-    };
-
-    this.checkLoggedIn = this.checkLoggedIn.bind(this);
-  }
-
-  checkLoggedIn() {
-    axios.get('/status')
-      .then((status) => {
-        this.setState({ isLoggedIn: status });
-      });
-  }
-
   render() {
-    return this.loggedIn ? (
-      <DashBoard />
-    ) :
-    (
-      <Login />
+    return (
+      <Router>
+        <div>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              fakeAuth.loggedIn ? (
+                <Redirect to="/dash" />
+              ) : (
+                <Master />
+              )
+            )}
+          />
+          <Route path="/dash" component={DashBoard} />
+        </div>
+      </Router>
     );
   }
 }
