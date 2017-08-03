b4b6c50b70b8db824a7382a63e1fdeb6f865640f
Alec Draymore <draymore26@gmail.com>
2017-08-03 10:41:36 -0400

    login and signup complete

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import axios from 'axios';
+import SignUp from './SignUp.jsx';
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       userAlreadyExists: false,
+      signUp: false,
     };
+
+    this.toggleSignUp = this.toggleSignUp.bind(this);
   }
-  handlePost(userObj) {
-    axios({
-      method: 'POST',
-      url: '/login',
-      data: userObj,
-    });
-    alert('POST REQUEST SENT TO SERVER');
+
+  toggleSignUp(e) {
+    e.preventDefault();
+    this.setState({ signUp: !this.state.signUp });
   }
 
   handleSubmit(e) {
@@ -22,49 +23,54 @@ class Login extends React.Component {
 
     const username = this.refs.username.value;
     const password = this.refs.password.value;
-    const user = {
-      username,
-      password,
-    };
-
-    alert('The username is: ' + username + ' The password is: ' + password);
-
-    this.handlePost(user);
+    axios.get('/login', {
+      params: { username, password },
+    })
+      .then((res) => {
+        console.log('res', res);
+        this.props.checkStatus();
+      });
   }
 
   render() {
+    if (!this.state.signUp) {
+      return (
+        <div>
+          <h1>Chimp Login</h1>
+          <form>
+            <label>
+              <br />
+                Please enter your username B
+              <br />
+              <input
+                id="inputUsername"
+                type="text"
+                placeholder="Enter chimp username"
+                autoFocus
+                ref="username"
+              />
+            </label>
+          </form>
+          <form onSubmit={this.handleSubmit.bind(this)} >
+            <label>
+              <br />
+                Bananas for passwords
+              <br />
+              <input
+                id="inputPassword"
+                type="text"
+                placeholder="Enter chimp password"
+                ref="password"
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          <button onClick={this.toggleSignUp}>Sign Up</button>
+        </div>
+      );
+    }
     return (
-      <div>
-        <h1>Chimp Login</h1>
-        <form>
-          <label>
-            <br />
-              Please enter your username B
-            <br />
-            <input
-              id="inputUsername"
-              type="text"
-              placeholder="Enter chimp username"
-              autoFocus
-              ref="username"
-            />
-          </label>
-        </form>
-        <form onSubmit={this.handleSubmit.bind(this)} >
-          <label>
-            <br />
-              Bananas for passwords
-            <br />
-            <input
-              id="inputPassword"
-              type="text"
-              placeholder="Enter chimp password"
-              ref="password"
-            />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
+      <SignUp toggleSignUp={this.toggleSignUp} />
     );
   }
 }
